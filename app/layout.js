1b2d4b0c5d6d4d7a64d8e37ea9b3c301d9b154f6
azdunczykowski <azdunczykowski@gmail.com>
2025-02-08 10:40:55 +0100

    added basic structure for setup the AuthJS, added Navbar

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,11 @@ import { Poppins } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import ThemeToggle from "../components/ThemeToggle";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const poppinsFont = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
+  subsets: ["sans-serif"],
   variable: "--font-poppins",
 });
 
@@ -16,12 +17,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={`${poppinsFont.variable} antialiased`}>
-        <ThemeProvider attribute="class">
-          <ThemeToggle />
-          {children}
-        </ThemeProvider>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
